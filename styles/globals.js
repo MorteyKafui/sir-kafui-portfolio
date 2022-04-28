dedfe9803af07ec8a69d1e13bf7f2638185299fa
mortey kafui <morteykafui@gmail.com>
2022-04-28 09:44:44 -0700

    initial setup

---
+++ styles/globals.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+
+
+
+html, body {
+  font-size: 62.5%;
+  height: 100%;
+}
+
+
+
+body {
+  font-family: 'Rubik', sans-serif;
+  font-size: 1.8rem;
+  line-height: 1.5;
+  -webkit-font-smoothing: antialiased;
+}
+
+
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+input, button, textarea, select {
+  font: inherit;
+}
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+#root, #__next {
+  isolation: isolate;
+}
+
+`;
