4d031e54346bf6b5ccb84cc30b43c1e42b5d304a
mortey kafui <morteykafui@gmail.com>
2022-04-28 23:19:11 -0700

    update footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -6,6 +6,7 @@ const Footer = () => {
     <StyledFooter id='footer'>
       <div className='footer-container'>
         <h4>✨little_coder ✨</h4>
+        <p>Ready to build with and for you 🔥</p>
         <p>
           &copy; {new Date().getFullYear()}{' '}
           <span>Everything is permissible! 😊</span>{' '}
