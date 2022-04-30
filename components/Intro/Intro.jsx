769c3d92f59e374817873ba2088548ea6802ec63
mortey kafui <morteykafui@gmail.com>
2022-04-29 21:11:15 -0700

    add animation

--- components/Intro/Intro.jsx
+++ components/Intro/Intro.jsx
@@ -1,20 +1,41 @@
+/* eslint-disable @next/next/link-passhref */
 /* eslint-disable react/no-unescaped-entities */
 import Link from 'next/link';
 import styles from './Intro.module.css';
+import { motion } from 'framer-motion';
 
 const Intro = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.headerContainer}>
+      <motion.div
+        className={styles.headerContainer}
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        transition={{ duration: 3 }}
+      >
         <h1>Hello World!</h1>
         <p>I'm Mortey_Kafui</p>
         <p>
           a self taught <span>Front-End Developer</span> 🤗
         </p>
         <Link href='#contact'>
-          <a>Contact me</a>
+          <motion.a
+            initial={{
+              x: '-100vw',
+            }}
+            animate={{
+              x: 0,
+            }}
+            transition={{
+              duration: 2,
+              type: 'spring',
+              stiffness: 300,
+            }}
+          >
+            Contact me
+          </motion.a>
         </Link>
-      </div>
+      </motion.div>
     </header>
   );
 };
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,16 +1,35 @@
 import Link from 'next/link';
 import styles from './Navbar.module.css';
 
+import { motion } from 'framer-motion';
+
+const animateNavbar = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: 'all 1s ease',
+  },
+};
+
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
       <div className={styles.container}>
         <div className={styles.navbarNav}>
-          <h1>
+          <motion.h1
+            initial={{
+              y: -100,
+              opacity: 0,
+            }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 2, type: 'spring', stiffness: 120 }}
+          >
             <Link href='/'>
               <a>Sir_Kafui</a>
             </Link>
-          </h1>
+          </motion.h1>
           <ul>
             <li>
               <Link href='/'>
