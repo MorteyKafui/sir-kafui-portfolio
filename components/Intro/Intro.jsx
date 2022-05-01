6139186bb7ee32bc3aa58ed8987fc11966a601ce
mortey kafui <morteykafui@gmail.com>
2022-05-01 11:20:53 -0700

    update animation

--- components/Intro/Intro.jsx
+++ components/Intro/Intro.jsx
@@ -11,7 +11,7 @@ const Intro = () => {
         className={styles.headerContainer}
         animate={{ opacity: 1 }}
         initial={{ opacity: 0 }}
-        transition={{ duration: 3 }}
+        transition={{ duration: 2 }}
       >
         <h1>Hello World!</h1>
         <p>I'm Mortey_Kafui</p>
@@ -21,15 +21,17 @@ const Intro = () => {
         <Link href='#contact'>
           <motion.a
             initial={{
-              x: '-100vw',
+              // x: '-100vw',
+              opacity: 0,
+              // y: '100vh',
             }}
             animate={{
-              x: 0,
+              // x: 0,
+              opacity: 1,
+              // y: 0,
             }}
             transition={{
-              duration: 2,
-              type: 'spring',
-              stiffness: 300,
+              duration: 3,
             }}
           >
             Contact me
